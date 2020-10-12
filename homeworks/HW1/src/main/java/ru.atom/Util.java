623a858cd3c91f981b12ada552910f56552fa3af
AlexErmolaev1998c <alex1.ermolaev1998@mail.ru>
2020-10-12 09:00:01 +0300

    released max and sum functions

--- homeworks/HW1/src/main/java/ru.atom/Util.java
+++ homeworks/HW1/src/main/java/ru.atom/Util.java
@@ -25,7 +25,13 @@ public class Util {
      * @return the largest of values.
      */
     public static int max(int[] values) {
-        throw new UnsupportedOperationException();
+        int max = values[0];
+        for (int i = 0; i < values.length; i++){
+            if (max < values[i]){
+                max = values[i];
+            }
+        }
+        return max;
     }
 
     /**
@@ -35,7 +41,11 @@ public class Util {
      * @return the sum of all values.
      */
     public static long sum(int[] values) {
-        throw new UnsupportedOperationException();
+        long sum = 0;
+        for (int i = 0; i < values.length; i++){
+            sum = sum + values[i];
+        }
+        return sum;
     }
 
 
