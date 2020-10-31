9700a063243cc4b56714b1b47ffe9982171264c1
AlexErmolaev1998c <alex1.ermolaev.1998@mail.ru>
2020-11-01 02:22:24 +0300

    bulls and cows game was released

--- homeworks/HW2/src/main/java/Game.java
+++ homeworks/HW2/src/main/java/Game.java
@@ -1,9 +1,7 @@
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Game {
@@ -13,38 +11,83 @@ public class Game {
     private List<String> words = new ArrayList<>();
     private String secretWord = "";
     private String pathToDictionary;
+    private int maxShotsNum = 10;
+    private int[] bullAndCows;
 
     public static void main(String[] args){
         Game bullsAndCows = new Game("dictionary.txt");
-        try {
-            bullsAndCows.loadDictionary();
-        } catch (IOException error){
-            // logger here
-        }
+        bullsAndCows.startGame();
     }
     public Game(String pathToDictionary){
 
         this.pathToDictionary = pathToDictionary;
+        try {
+            this.loadDictionary();
+        } catch (IOException error){
+            log.log(Level.WARNING, error.getMessage());
+        }
     }
     private void startGame(){
-
+        log.info("Game was started");
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to Bulls and Cows game!");
+        while (true) {
+            this.secretWord = makeSecretWord();
+            log.info("Secret word was made. It is " + this.secretWord);
+            int usedShotsNum = 0;
+            while (usedShotsNum < maxShotsNum) {
+                System.out.println("Can you guess my secret word? Its length is " + this.secretWord.length() + ". " + "You got " + (maxShotsNum - usedShotsNum) + " shots.");
+                String userString = scanner.next();
+                if (userString.length() != this.secretWord.length()){
+                    System.out.println("Lengths of your word and mine are different.");
+                    continue;
+                }
+                log.info("User tried to know the secret word. Shots used: " + (usedShotsNum+1));
+                if (userString.equals(secretWord)) {
+                    System.out.println("Congratulations! You won!");
+                    log.info("User won");
+                    break;
+                } else {
+                    this.bullAndCows = countBullsAndCows(userString);
+                    System.out.println("Nice try. You almost guessed my secret word. Bulls: " + this.bullAndCows[0] + ", Cows: " + this.bullAndCows[1] + ".");
+                    usedShotsNum++;
+                    if (usedShotsNum == maxShotsNum){
+                        System.out.println("You used all available shots and lost the game :c.");
+                        log.info("User lost");
+                    }
+                }
+            }
+            System.out.println("Do you wanna play again? Y/N");
+            if (!scanner.next().equals("Y")){
+                log.info("User stopped playing");
+                break;
+            }
+            log.info("User started new game");
+        }
     }
 
-    protected void loadDictionary() throws IOException {
+    private void loadDictionary() throws IOException {
         FileReader fileReader = new FileReader(pathToDictionary);
         Scanner scan = new Scanner(fileReader);
         while (scan.hasNextLine()){
             words.add(scan.nextLine());
         }
         fileReader.close();
+        log.info("Dictionary was loaded");
     }
-    protected int countBulls(String userString){
-
-        return 0;
+    private String makeSecretWord(){
+        int seed = (int)(Math.random()*(this.words.size()-1));
+        return this.words.get(seed);
     }
-    protected int countCows(String userString){
-
-        return 0;
+    private int[] countBullsAndCows(String userString){
+        int[] bullsAndCows = {0,0};
+        for (int i = 0; i < userString.length(); i++){
+            if (userString.charAt(i) == secretWord.charAt(i)){
+                bullsAndCows[0]++;
+            } else if(secretWord.indexOf(userString.charAt(i)) != -1){
+                bullsAndCows[1]++;
+            }
+        }
+        return bullsAndCows;
     }
 }
